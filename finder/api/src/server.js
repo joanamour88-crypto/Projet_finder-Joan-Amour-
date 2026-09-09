f2443310bbc98bd0323ff90603d4e4e254b1fd43
JoanAmourRabelais <joan.amour88@gmail.com>
2026-09-09 15:40:06 +0200

    étape 1 terminée

--- finder/api/src/server.js
+++ finder/api/src/server.js
@@ -13,8 +13,11 @@ app.get('/', (req, res) => {
     res.json({ message: 'API en ligne' });
 });
 
-app.get('/chambres', (req, res) => res.json(chambres));
+//app.get('/chambres', (req, res) => res.json(chambres));
 app.get('/hotels', (req, res) => res.json(hotels));
+app.get('/comptes', (req, res) => res.json(comptes));
+app.get('/reservations', (req, res) => res.json(reservations));
+
 
 app.get('/chambres/:id', (req, res) => {
     const id = Number(req.params.id);
@@ -25,6 +28,19 @@ app.get('/chambres/:id', (req, res) => {
     res.json(chambre);
 });
 
+app.get('/chambres', (req, res) => {
+    const prix = Number(req.query.prix_max);
+    const chambre = chambres.filter(chambre => chambre.prix_nuit <= prix);
+    if (!chambre) { 
+        return res.status(404).json({ error: 'Chambre non trouvée' });
+    }
+    if (isNaN(prix) /*|| chambre.length === 0*/) {
+        return res.status(400).json({ error: 'Le prix doit être un nombre' });
+    }
+    res.json(chambre);
+});
+
+
 app.get('/hotels/:id', (req, res) => {
     const id = Number(req.params.id);
     const hotel = hotels.find(hotel => hotel.id === id);
@@ -34,6 +50,32 @@ app.get('/hotels/:id', (req, res) => {
     res.json(hotel);
 });
 
+app.get('/comptes/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const compte = comptes.find(compte => compte.id === id);
+    if (!compte) { 
+        return res.status(404).json({ error: 'Compte non trouvé' });
+    }
+    res.json(compte);
+});
+
+app.get('/reservations/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const reservation = reservations.find(reservation => reservation.id === id);
+    if (!reservation) { 
+        return res.status(404).json({ error: 'Réservation non trouvée' });
+    }
+    res.json(reservation);
+});
+
+app.get('/hotels/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const hotel = hotels.find(hotel => hotel.id === id);
+    if (!hotel) { 
+        return res.status(404).json({ error: 'Hôtel non trouvé' });
+    }
+    res.json(hotel);
+});
 const PORT = 3000;
 
 app.listen(PORT, () => {
